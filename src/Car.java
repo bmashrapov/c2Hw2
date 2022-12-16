a4d5dfd7b29e8c2e095496a0dc3609e66edb59fd
b.Mashrapov <b_mashrapov@mail.ru>
2022-12-16 13:06:12 +0100

    2

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,170 @@
-package PACKAGE_NAME;public class Car {
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Car {
+    final String brand;
+    final String model;
+    double engineVolume;
+    String color;
+    int year;
+    final String country;
+    String transmission;
+    String bodyType;
+    String registrationNumber;
+    final int capacity;
+    String typTires;
+
+    Car(String brand, String model, double engineVolume, String color, String country, String transmission, String bodyType, String registrationNumber, int capacity, String typTires) {
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (country == null) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (transmission == null) {
+            this.transmission = "механика";
+        } else {
+            this.transmission = transmission;
+        }
+        if (bodyType == null) {
+            this.bodyType = "седан";
+        } else {
+            this.transmission = transmission;
+        }
+        if (registrationNumber == null) {
+            this.registrationNumber = "Введите значение";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+        if (capacity == 0) {
+            this.capacity = 4;
+        } else {
+            this.capacity = capacity;
+        }
+        if (typTires == null) {
+            this.typTires = "Введите значение";
+        } else {
+            this.typTires=typTires;
+        }
+    }
+    private static String checkTypeTires(String typTires) {
+        String typTires1 = "";
+        Date date = new Date();
+        LocalDate localDate = LocalDate.now();
+        int month = localDate.getMonthValue();
+        if (month > 9 || month < 4) {
+            typTires1 = "зимние";
+        } else {
+            typTires1 = "летние";
+        }
+        return typTires1;
+    }
+
+    public static boolean changeTires(String typTires) {
+        if (typTires.equalsIgnoreCase(checkTypeTires(typTires))) {
+            typTires = typTires;
+        } else {
+            System.out.println("Смените резину на сезонную");
+        }
+        return typTires.equalsIgnoreCase(checkTypeTires(typTires));
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String isTypTires() {
+        return typTires;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setTypTires(String typTires) {
+        this.typTires = typTires;
+    }
+
+    public void printCar() {
+        System.out.println("Машина " + brand + " " + model + ". Объем двигателя " + engineVolume + " литра." + " Цвет - " + color + ". Страна - производитель " + country);
+    }
+}
+
+class Key {
+    private boolean remoteEngineStart;
+    private boolean keylessAccess;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("Lada", "Granta", 1.7,"жёлтый", "Россия", "механика","седан","СР735ОП",4,"летние");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", "Германия", "механика","седан","СР735ОП",4,"летние");
+        Car BMW = new Car("BMW", "Z8", 3.0, "черный", "Германия", "механика","седан","СР735ОП",4,"летние");
+        Car kia = new Car("Kia", "Sportrage 4-го поколения", 2.4, "красный", "Южная Корея", "механика","седан","СР735ОП",4,"летние");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", "Южная Корея", "механика","седан","СР735ОП",4,"летние");
+        lada.printCar();
+        audi.printCar();
+        BMW.printCar();
+        kia.printCar();
+        hyundai.printCar();
     }
 }
